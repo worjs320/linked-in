c67ca345e428d34f9756531930fc0c3a6be08773
worjs320 <worjs320@naver.com>
2021-01-20 00:36:30 +0900

    feat: Work header and headerOption

--- src/Header.js
+++ src/Header.js
@@ -1,22 +1,36 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
+import LinkdinIcon from "./img/linkedin.svg";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 function Header() {
   return (
     <div className="header">
       <div className="header__left">
-        <img
-          src="https://www.flaticon.com/svg/vstatic/svg/174/174857.svg?token=exp=1611038251~hmac=755187367e7240f1740af8b8bd79684c"
-          alt=""
-        />
+        <img src={LinkdinIcon} alt="" />
         <div className="header__search">
           <SearchIcon />
           <input type="text" />
         </div>
       </div>
 
-      <div className="header__right"></div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="NotificationsIcon" />
+        <HeaderOption
+          avatar="https://lh3.googleusercontent.com/a-/AOh14GjQYkKz0gGfyEspvZZLgkj1igFiVP4Zfuefk2OG=s96-c"
+          title="me"
+        />
+      </div>
     </div>
   );
 }
