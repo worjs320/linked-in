e269221e8322a118233467443e020d62ac5d4905
worjs320 <worjs320@naver.com>
2021-01-21 22:10:56 +0900

    feat: Add Sidebar component, Edit App.css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      {/* {Header} */}
+    <div className="app">
       <Header />
-      {/* {App body} */}
+      <div className="app__body">
+        <Sidebar />
+      </div>
       {/* {Feed} */}
       {/* {Widgets} */}
     </div>
