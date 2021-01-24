8e45cfa6a1142f21dc7a2e87d431ee9c4d4711f8
worjs320 <worjs320@naver.com>
2021-01-24 21:45:41 +0900

    feat: Work redux store, action, ruducer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
 import "./App.css";
+import Login from "./Login";
 import Feed from "./Feed";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
       <Header />
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-      </div>
-      {/* {Widgets} */}
+
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+          {/* {Widgets} */}
+        </div>
+      )}
     </div>
   );
 }
