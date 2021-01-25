86ab69b528ac7b99f896d38e35f1b8cf2aab59ce
worjs320 <worjs320@naver.com>
2021-01-25 22:53:24 +0900

    feat: Work sign up logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { selectUser } from "./features/userSlice";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
 import "./App.css";
 import Login from "./Login";
 import Feed from "./Feed";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import { auth } from "./firebase";
 
 function App() {
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+            photoURL: userAuth.photoURL,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, []);
 
   return (
     <div className="app">
--- src/Login.js
+++ src/Login.js
@@ -1,19 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import LinkdinLogo from "./img/linkedinLogo.png";
+import { auth } from "./firebase";
+import { useDispatch } from "react-redux";
+import { login } from "./features/userSlice";
 
 function Login() {
-  const loginToApp = () => {};
-  const register = () => {};
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [profilePic, setProfilePic] = useState("");
+  const dispatch = useDispatch();
+
+  const loginToApp = (e) => {
+    e.preventDefualt();
+  };
+
+  const register = () => {
+    if (!name) {
+      return alert("Please enter a full name!");
+    }
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userAuth) => {
+        userAuth.user
+          .updateProfile({
+            displayName: name,
+            photoURL: profilePic,
+          })
+          .then(() => {
+            dispatch(
+              login({
+                email: userAuth.user.email,
+                uid: userAuth.user.uid,
+                displayName: name,
+                photoURL: profilePic,
+              })
+            );
+          });
+      })
+      .catch((error) => alert(error));
+  };
 
   return (
     <div className="login">
       <img src={LinkdinLogo} alt="" />
       <form>
-        <input type="text" placeholder="Full name" />
-        <input type="text" placeholder="Profile pic URL" />
-        <input type="Email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="Full name"
+        />
+        <input
+          value={profilePic}
+          onChange={(e) => setProfilePic(e.target.value)}
+          type="text"
+          placeholder="Profile picture URL"
+        />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="Email"
+          placeholder="Email"
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password"
+        />
         <button type="submit" onClick={loginToApp}>
           Sign In
         </button>
